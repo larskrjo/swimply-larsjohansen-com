fa0749fe94a8ff8c0c2808286e76f35560664333
Lars Johansen <larskrjo@gmail.com>
2025-10-12 15:08:22 -0700

    Adding dev mode

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,13 +4,20 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.job.data_worker import DataWorker
 from app.api.pool_temperature_api import *
 
+import os
+
+if os.getenv("DEVELOPMENT_MODE") == "prod":
+    allowed_origins = "https://swimply.larsjohansen.com"
+else:
+    allowed_origins = "http://localhost:5173"
+
 app = FastAPI()
 
 app.include_router(pool_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[allowed_origins],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
