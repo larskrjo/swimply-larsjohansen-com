b74627fbdea669c0e914c5d22c83f1ea5a8524a7
Lars Johansen <larskrjo@gmail.com>
2025-11-05 12:20:09 -0800

    more things

--- frontend/app/routes/index.tsx
+++ frontend/app/routes/index.tsx
@@ -19,8 +19,7 @@ export async function clientLoader() {
         if (item.max > max) max = item.max;
     })
 
-    return {rows: [], min: 0, max: 0};
-    // return {rows, min, max};
+    return {rows, min, max};
 }
 
 
